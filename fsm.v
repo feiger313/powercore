719a4559d99720f74b09c35d33d19bbe1f265bda
powercore <2212289801@qq.com>
2018-02-24 19:02:58 +0800

    Add files via upload

---
+++ fsm.v
@@ -0,0 +1,21 @@
+//==============================================
+//   Filename       :
+//   Author         :liuxf
+//   Description    :
+//   Called by      :
+//   History        :
+//   Email          :liuxf000@163.com
+//   Company        :xxxx
+//   Copyright(c) 2018,xxxx Technology Inc,All right reserved
+//
+//   //==============================================
+//   
+
+module fsm (
+
+
+
+)
+
+
+endmodule
